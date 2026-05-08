ff23767a268958d65fe52de5373f284d2043ed9c
Leonardo Lima de Vasconcellos <leonardolimadevasconcellos@gmail.com>
2026-05-08 15:17:34 -0300

    fix: prevent browser / next warning about scroll-behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
     <html
       lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} h-full antialiased`}
+      data-scroll-behavior="smooth"
     >
       <body className="min-h-full flex flex-col">
         <div id="viewport" className="viewport">
